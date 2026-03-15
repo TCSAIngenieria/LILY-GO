94e3c201bd0ca3ec8c6487566953afcbc25ed388
TCSAIngenieria <136738116+TCSAIngenieria@users.noreply.github.com>
2026-03-15 11:11:24 -0300

    fix(mqtt): forzar reconexion mqtt al cambiar id de dispositivo y actualizar version

--- main/main.ino
+++ main/main.ino
@@ -48,7 +48,7 @@ HardwareSerial SensorSerial(2); // UART2
 #define LED_PIN 2
 #define WDT_TIMEOUT 120 // segundos para que reinicie por watchdog
 
-String versionado = "V02.02.04";
+String versionado = "V02.02.05";
 
 /*VARIABLES MQTT*/
 unsigned long ledTimer = 0;
